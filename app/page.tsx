4e0498c97bfca739adb6b2d26e5f7c981f6d7d58
Thanarat Ruengkum <ninenights19534@gmail.com>
2024-11-06 20:20:50 +0700

    Use state

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+import { Button, Input, Text } from "@mantine/core";
+import { useState } from "react";
 export default function Home() {
+  const [name, setname] = useState("Thanarat");
   return (
     <>
-      <span>abc</span>
+      <Text
+        size="xl"
+        fw={900}
+        variant="gradient"
+        gradient={{ from: "blue", to: "cyan", deg: 90 }}
+      >
+        {name}
+      </Text>
+      <Input
+        variant="filled"
+        placeholder="Tasks"
+        onChange={(e) => setname(e.target.value)}
+      />
+      <Button variant="filled">Add Task</Button>
     </>
   );
 }
